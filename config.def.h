27e1f112aa76ea71a41ddc34f995a1932034caee
ZaheenJ <zaheenjamil@gmail.com>
2020-07-18 17:03:31 -0700

    Made font slightly smaler

--- config.def.h
+++ config.def.h
@@ -9,8 +9,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int minwsz    = 20;       /* Minimal heigt of a client for smfact */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "FiraCode Nerd Font:size=12" };
-static const char dmenufont[]       = "Fira Code:size=12";
+static const char *fonts[]          = { "FiraCode Nerd Font:size=11" };
+static const char dmenufont[]       = "Fira Code:size=11";
 static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#222222";
 static const char col_gray3[]       = "#eeeeee";
